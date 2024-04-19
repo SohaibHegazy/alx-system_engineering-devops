313db6a96f7789cdff79121a5c41b732072de2ce
“SohaibHegazy” <sohaib.mohamed@hotmail.com>
2024-04-19 14:37:00 -0700

    modify

--- 0x0A-configuration_management/1-install_a_package.pp
+++ 0x0A-configuration_management/1-install_a_package.pp
@@ -1,6 +1,11 @@
 #Using Puppet, install flask from pip3
+package { 'werkzeug':
+  ensure =>'installed',
+}
+
 package { 'flask':
   ensure   =>'2.1.0',
   provider =>'pip3',
+  before  =>'werkzeug',
 }
 
