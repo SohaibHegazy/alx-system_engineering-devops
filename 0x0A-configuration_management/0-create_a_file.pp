cac6caf716dca5f21ca15ecb38563ce681bb2b8f
“SohaibHegazy” <sohaib.mohamed@hotmail.com>
2024-04-19 13:59:25 -0700

    modify

--- 0x0A-configuration_management/0-create_a_file.pp
+++ 0x0A-configuration_management/0-create_a_file.pp
@@ -1,10 +1,10 @@
 #Using Puppet, create a file in /tmp
 file { '/tmp/school':
 ensure  =>'file',
-path  =>'/tmp/school',
-content  =>'I love Puppet',
-mode  =>'0744',
-owner  =>'www-data',
-group  =>'www-data',
+path    =>'/tmp/school',
+content =>'I love Puppet',
+mode    =>'0744',
+owner   =>'www-data',
+group   =>'www-data',
 }
 
