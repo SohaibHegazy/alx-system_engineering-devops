f1240650e6c64122a58fe61c4c4ca0d831df5efb
Sohaib Hegazy <sohaib.mohamed@hotmail.com>
2024-06-04 11:24:04 -0700

    modify

--- 0x16-api_advanced/0-subs.py
+++ 0x16-api_advanced/0-subs.py
@@ -18,7 +18,7 @@ def number_of_subscribers(subreddit):
 
     try:
         response = requests.get(url, headers=headers, allow_redirects=False)
-        if response.status_code == 200:
+        if response.status_code === 200:
             data = response.json()
             return data['data']['subscribers']
         else:
