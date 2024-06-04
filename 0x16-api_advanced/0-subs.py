55f56290fde1a0a249555750aa25034f483d82ec
Sohaib Hegazy <sohaib.mohamed@hotmail.com>
2024-06-04 11:29:57 -0700

    modify

--- 0x16-api_advanced/0-subs.py
+++ 0x16-api_advanced/0-subs.py
@@ -13,15 +13,14 @@ def number_of_subscribers(subreddit):
     a function to get the total number of subscribers
     in Reddit, it returns 0 if invalid subbreddit is passed
     '''
-    url = f"https://www.reddit.com/r/{subreddit}/about"
+    url = f"https://www.reddit.com/r/{subreddit}/about.json"
     headers = {'User-Agent': 'CustomClient/1.0'}
-
-    try:
-        response = requests.get(url, headers=headers, allow_redirects=False)
-        if response.status_code == 200:
-            response = response.json()
-            return (response.get('data').get('subscribers'))
-        else:
-            return (0)
-    except requests.RequestException:
-        return (0)
+    response = requests.get(url, headers=headers, allow_redirects=False)
+        
+    if response.status_code != 200:
+      return (0)
+    response = response.json()
+    if 'data' in response:
+      return (response.get('data').get('subscribers'))
+    else:
+      return (0)
