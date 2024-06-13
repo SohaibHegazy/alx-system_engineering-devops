cf6e3dc4adf7ffad181f9499b7db8ad161e04bf8
Sohaib Hegazy <sohaib.mohamed@hotmail.com>
2024-06-13 01:55:15 -0700

    modify

--- 0x1B-web_stack_debugging_4/1-user_limit.pp
+++ 0x1B-web_stack_debugging_4/1-user_limit.pp
@@ -2,12 +2,12 @@
 
 #Fix soft files limit
 exec {'set soft files limit'
-  command => 'sed -i "/holberton soft/s/4/9000/" /etc/security/limits.conf',
+  command => 'sed -i "/holberton soft/s/4/10000/" /etc/security/limits.conf',
   path    => '/usr/local/bin/:/bin/'
 }
 
 #Fix hard files limit
-exec {'set soft files limit'
+exec {'set hard files limit'
   command => 'sed -i "/holberton hard/s/5/10000/" /etc/security/limits.conf',
   path    => '/usr/local/bin/:/bin/'
 }
