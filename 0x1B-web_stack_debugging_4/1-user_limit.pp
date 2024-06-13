9f28b5ba4a2519d854e1d7f48ed8fe53c8e6cbc5
Sohaib Hegazy <sohaib.mohamed@hotmail.com>
2024-06-13 01:53:29 -0700

    modify

---
+++ 0x1B-web_stack_debugging_4/1-user_limit.pp
@@ -0,0 +1,13 @@
+#configuration so that it is possible to login with the holberton user and open a file
+
+#Fix soft files limit
+exec {'set soft files limit'
+  command => 'sed -i "/holberton soft/s/4/10000/" /etc/security/limits.conf',
+  path    => '/usr/local/bin/:/bin/'
+}
+
+#Fix hard files limit
+exec {'set soft files limit'
+  command => 'sed -i "/holberton hard/s/5/10000/" /etc/security/limits.conf',
+  path    => '/usr/local/bin/:/bin/'
+}
